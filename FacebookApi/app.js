e0a34c0dd7c6d62a6144a4295c9ee20ba28ce5f7
Milhousepaulojean <paulo_jean25@hotmail.com>
2018-07-12 18:31:00 -0300

    Teste Publish 5

--- FacebookApi/app.js
+++ FacebookApi/app.js
@@ -1,9 +1,9 @@
 var http = require('http')
--var porta = 80
-- 
--http.createServer((req, res) => {
--  res.writeHead(200, {'Content-Type': 'text/plain'})
--  res.end('Hello Node')
--}).listen(porta)
-- 
--console.log('Servidor rodando na porta: '+ porta) 
+var porta = 80
+ 
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello Node')
+}).listen(porta)
+ 
+console.log('Servidor rodando na porta: '+ porta) 
