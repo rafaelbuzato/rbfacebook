90f08d54b443ca8e320c49202b3c06f01bf7477c
Milhousepaulojean <paulo_jean25@hotmail.com>
2018-07-12 11:02:22 -0300

    Create Folder Publish

--- FacebookApi/V3/scripts/function.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-					$.getJSON( "/appsFriends", function( json ) {
-						var friends = jQuery.parseJSON(json)
-						
-						for(var i in friends){
-								$("#iptnamefriends").append("<option>"+friends[i].fullName+"</option>")						
-						}						
-					});
-			});
