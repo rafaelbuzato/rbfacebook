6f12cc1cd8cf75753b12c29d32e800d7cad39e5b
Milhousepaulojean <paulo_jean25@hotmail.com>
2018-07-13 17:45:26 -0300

    Folder old

--- FacebookApi/app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-var porta = 21004
- 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello Node New try!')
-}).listen(porta)
- 
-console.log('Servidor rodando na porta: '+ porta)
